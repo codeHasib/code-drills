c52bef6f215ccc4526d6effc17d38512a9208173
codeHasib <mohammadhasib9827@gmail.com>
2026-06-05 01:34:05 +0600

    db connected

--- image_uploading_system_backend/index.js
+++ image_uploading_system_backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 dotenv.config();
 
@@ -9,8 +10,32 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = 5000;
-app.get("/", (req, res) => {
-  res.send("hello");
+
+const uri = `${process.env.DB_URI}`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("Image_Uploading_System");
+    const imageCollections = db.collection("images");
+
+    app.get("/", async (req, res) => {
+      const cursor = imageCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(PORT, console.log(`PORT is listening on ${PORT}`));
