b1bba52dd6ff12641b05561c458c5180a0c7bf88
codeHasib <mohammadhasib9827@gmail.com>
2026-06-11 19:44:12 +0600

    add html questions and answers

---
+++ interview_practice/practiceHtml.js
@@ -0,0 +1,6 @@
+// Difference between figure and image tag
+// figure tag is a section tag which keeps a section divided while image tag is usually used for adding image to html file
+// Difference between inline and block?
+// inline element just takes the width as much as it needed like span tag anchor tag while block elements takes full width of screen like div tag and h1 tag and we can provide height width padding and all to block element but cant in inline elements.
+// What is required attribute?
+// required attribute is used in input fields for ensuring the user cant escape those inputs and submit and where the required attribute is mentioned user need to fill up that input field
