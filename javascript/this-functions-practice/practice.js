16eaec0b2273fc2c499d556564353cd174a3ccf4
codeHasib <mohammadhasib9827@gmail.com>
2026-06-12 20:59:28 +0600

    practiced call with challenge

---
+++ javascript/this-functions-practice/practice.js
@@ -0,0 +1,36 @@
+// 🟢 Level 1 — Basics (Warm-up)
+// ✅ Challenge 1: Fix this
+const user = {
+  name: "Hasib",
+};
+
+function sayHi() {
+  console.log("Hi " + this.name);
+}
+
+// // ❌ Currently not working
+// sayHi();
+
+// 👉 Task: Use call to make it print:
+
+// Hi Hasib
+
+sayHi.call(user);
+
+// ✅ Challenge 2: Pass Arguments
+// const user = {
+//   name: "Hasib"
+// };
+
+// function introduce(age, city) {
+//   console.log(`I am ${this.name}, ${age}, from ${city}`);
+// }
+
+// 👉 Task:
+
+// Use call
+// Then use apply (same result)
+
+// Expected:
+
+// I am Hasib, 22, from Chittagong
