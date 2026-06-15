718b4b15a10c279ace5788e716ba505fdc5e5e96
codeHasib <mohammadhasib9827@gmail.com>
2026-06-15 11:33:34 +0600

    solved the userdetailspage problem

---
+++ interview_practice/with-israt-apu/interview4.js
@@ -0,0 +1,27 @@
+// export const UserDetailsPage = () => {
+//   const { id } = useParams();
+//   return (
+//     <>
+//       <p>/users/{id}</p>
+//     </>
+//   );
+// };
+
+// 6. Find the first element that meets condition
+// javascript
+// Input: ([10, 15, 20, 25], num => num > 18)
+// Output: 20
+
+// const arr = [10, 15, 20, 25];
+// const firstOne = arr.find(num => num > 18);
+// console.log(firstOne);
+
+// Capitalize first letter of each word
+// javascript
+// Input: "hello world from react"
+// Output: "Hello World From React"
+// function capitalize(str) {
+//   let strArr = str.split(" ").map(word=> word[0].toUpperCase() + word.slice(1)).join(" ");
+//   console.log(strArr);
+// }
+// capitalize("hello world from react");
