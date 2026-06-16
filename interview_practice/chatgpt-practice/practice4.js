d4fef627469ae6e5d7e08b9f37ae0ebf67bfa92f
codeHasib <mohammadhasib9827@gmail.com>
2026-06-16 20:46:57 +0600

    solved the outOfStockProducts problem

--- interview_practice/chatgpt-practice/practice4.js
+++ interview_practice/chatgpt-practice/practice4.js
@@ -12,25 +12,25 @@ const users = [
 
 // Create:
 
-function getUserReport(users) {
-  return users.reduce(
-    (acc, curr) => {
-      acc.totalUsers = users.length;
-      if (curr.active) {
-        acc.activeUsers++;
-        acc.activeNames.push(curr.name);
-      } else if (!curr.active) acc.inactiveUsers++;
-      return acc;
-    },
-    {
-      totalUsers: 0,
-      activeUsers: 0,
-      inactiveUsers: 0,
-      activeNames: [],
-    },
-  );
-}
-console.log(getUserReport(users));
+// function getUserReport(users) {
+//   return users.reduce(
+//     (acc, curr) => {
+//       acc.totalUsers = users.length;
+//       if (curr.active) {
+//         acc.activeUsers++;
+//         acc.activeNames.push(curr.name);
+//       } else if (!curr.active) acc.inactiveUsers++;
+//       return acc;
+//     },
+//     {
+//       totalUsers: 0,
+//       activeUsers: 0,
+//       inactiveUsers: 0,
+//       activeNames: [],
+//     },
+//   );
+// }
+// console.log(getUserReport(users));
 // Return:
 
 // {
@@ -39,3 +39,28 @@ console.log(getUserReport(users));
 //   inactiveUsers: 2,
 //   activeNames: ["Hasib", "Sarah", "Alex"]
 // }
+
+// Challenge 2 — E-commerce Inventory Check
+
+// Given:
+
+const products = [
+  { id: 1, name: "Laptop", stock: 10 },
+  { id: 2, name: "Phone", stock: 0 },
+  { id: 3, name: "Keyboard", stock: 5 },
+  { id: 4, name: "Monitor", stock: 0 },
+];
+
+// Create:
+
+function getOutOfStockProducts(products) {
+  return products.reduce((acc, curr) => {
+    !curr.stock ? acc.push(curr.name) : "";
+    return acc;
+  }, []);
+}
+
+console.log(getOutOfStockProducts(products));
+// Return:
+
+// ["Phone", "Monitor"]
