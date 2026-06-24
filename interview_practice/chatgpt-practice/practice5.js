6b5370cdca0bab372489f75ed1262d79591a808b
codeHasib <mohammadhasib9827@gmail.com>
2026-06-24 21:56:30 +0600

    solved the capitalize names problem

--- interview_practice/chatgpt-practice/practice5.js
+++ interview_practice/chatgpt-practice/practice5.js
@@ -2,15 +2,15 @@
 
 // Given:
 
-const nums = [10, 5, 20, 8, 15];
-function getSecondMax(arr) {
-  const firstMaxNum = Math.max(...arr);
-  const indexOfFirst = nums.indexOf(firstMaxNum);
-  arr.splice(indexOfFirst, 1);
-  const secondMaxNum = Math.max(...arr);
-  return secondMaxNum;
-}
-console.log(getSecondMax(nums));
+// const nums = [10, 5, 20, 8, 15];
+// function getSecondMax(arr) {
+//   const firstMaxNum = Math.max(...arr);
+//   const indexOfFirst = nums.indexOf(firstMaxNum);
+//   arr.splice(indexOfFirst, 1);
+//   const secondMaxNum = Math.max(...arr);
+//   return secondMaxNum;
+// }
+// console.log(getSecondMax(nums));
 // Return:
 
 // 15
@@ -19,3 +19,19 @@ console.log(getSecondMax(nums));
 
 // Reusable function
 // Works for any array length
+
+// Challenge 2 — Capitalize Names
+
+// Given:
+
+const names = ["hasib", "john", "sarah"];
+
+function capitalizeNames(arr) {
+  let newArr = arr.map((item) => item[0].toUpperCase() + item.slice(1));
+  return newArr;
+}
+console.log(capitalizeNames(names));
+
+// Return:
+
+// ["Hasib", "John", "Sarah"]
