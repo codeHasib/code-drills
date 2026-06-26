3bbe171a595cca4e682e84ac9a7b3f1048b091f8
codeHasib <mohammadhasib9827@gmail.com>
2026-06-26 19:38:33 +0600

    solved the longest word problem

--- interview_practice/chatgpt-practice/practice6.js
+++ interview_practice/chatgpt-practice/practice6.js
@@ -2,11 +2,29 @@
 
 // Given:
 
-const arr = [0, "hello", false, "", 42, null, undefined, "world"];
+// const arr = [0, "hello", false, "", 42, null, undefined, "world"];
 
-const freshArr = arr.filter((item) => item);
+// const freshArr = arr.filter((item) => item);
 
-console.log(freshArr);
+// console.log(freshArr);
 // Return:
 
 // ["hello", 42, "world"]
+
+// Challenge 2 — Longest Word
+
+// Given:
+
+const sentence = "JavaScript developers solve interesting problems";
+
+const longestWord = sentence
+  .split(" ")
+  .reduce((longestWord, curr) =>
+    longestWord.length > curr.length ? longestWord : curr,
+  );
+
+console.log(longestWord);
+
+// Return:
+
+// "interesting"
