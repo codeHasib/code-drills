0231f8bc79afea7198513a65712bb5ae7707fb4b
codeHasib <mohammadhasib9827@gmail.com>
2026-06-26 20:32:18 +0600

    solved the secondLargestNum problem

--- interview_practice/chatgpt-practice/practice6.js
+++ interview_practice/chatgpt-practice/practice6.js
@@ -15,16 +15,41 @@
 
 // Given:
 
-const sentence = "JavaScript developers solve interesting problems";
+// const sentence = "JavaScript developers solve interesting problems";
 
-const longestWord = sentence
-  .split(" ")
-  .reduce((longestWord, curr) =>
-    longestWord.length > curr.length ? longestWord : curr,
-  );
+// const longestWord = sentence
+//   .split(" ")
+//   .reduce((longestWord, curr) =>
+//     longestWord.length > curr.length ? longestWord : curr,
+//   );
 
-console.log(longestWord);
+// console.log(longestWord);
 
 // Return:
 
 // "interesting"
+
+// Round 1 - JavaScript (Easy)
+// Question 1
+
+// Write a function that returns the second largest number.
+
+const arr = [10, 5, 7, 20, 15];
+
+function secondLargest(arr) {
+  const largestNum = Math.max(...arr);
+  const newArr = arr.filter((item) => item !== largestNum);
+  const secondLargestNum = Math.max(...newArr);
+  return secondLargestNum;
+}
+
+console.log(secondLargest(arr));
+
+// Expected:
+
+// 20 -> largest
+// 15 -> second largest
+
+// return 15;
+
+// Don't use sort().
